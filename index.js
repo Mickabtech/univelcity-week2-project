9d3220b66e11784dff1915e73e9ed6538b3bd8e0
Mickab <mickabtech@gmail.com>
2024-04-02 08:28:49 +0100

    hamburger function

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-  const navLinks = document.querySelector(".nav-links");
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links');
 
-  // Toggle nav links when hamburger menu is clicked
-  hamburgerMenu.addEventListener("click", function() {
-      navLinks.classList.toggle("show");
+  // Toggle nav links and icon when hamburger menu is clicked
+  hamburgerMenu.addEventListener('click', function() {
+      navLinks.classList.toggle('show');
+      hamburgerMenu.classList.toggle('active');
   });
 });
